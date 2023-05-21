2886a946817c2a430b5109483dffe6e532e6859b
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-05-21 18:16:18 +0800

    Updated: Sun May 21 18:16:18 CST 2023

--- update_and_deploy.sh
+++ update_and_deploy.sh
@@ -5,8 +5,14 @@ git reset --hard origin/main
 docker-compose down -v --remove-orphans
 docker-compose pull
 docker-compose up -d --no-build --remove-orphans
+# if success
+if [ $? -eq 0 ]; then
+    echo "Success"
+    docker image prune -a --force
+    docker container prune --force
+else
+    echo "Failed"
+fi
 
-docker image prune -a --force
-docker container prune --force
 
 #docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
