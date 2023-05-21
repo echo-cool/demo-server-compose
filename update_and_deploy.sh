97d495e6627598bd7ba820e4c812a58b1e3662e7
Wang Yuyang <yuyang.wang@ucdconnect.ie>
2023-05-21 19:48:37 +0800

    Updated: Sun May 21 19:48:37 CST 2023

--- update_and_deploy.sh
+++ update_and_deploy.sh
@@ -1,7 +1,13 @@
 git checkout main
 git fetch --all
 git reset --hard origin/main
-
+docker-compose config -q
+if [ $? -eq 0 ]; then
+    echo "Config is valid"
+else
+    echo "Config is invalid"
+    exit 1
+fi
 docker-compose down -v --remove-orphans
 docker-compose pull
 docker-compose up -d --no-build --remove-orphans
